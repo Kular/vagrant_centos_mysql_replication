459afe5ec71c61ebc355cc8769eebe3a78a5e7dc
Xiang Li <xiang.li@dena.com>
2014-07-04 17:33:56 +0900

    commit before merge

--- site-cookbooks/Apache/recipes/default.rb
+++ site-cookbooks/Apache/recipes/default.rb
@@ -7,16 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Apache2
-package "httpd" do
-	action :install
+# Apache2 
+%w{httpd httpd-devel}.each do |pkg|
+	package pkg do
+		action :install
+	end
 end
 
-# Apacheを起動
+# Apacheを起動 & 自動起動
 service "httpd" do
     action [:start, :enable]
-end
-
-bash "chkconfig" do
-	code "chkconfig httpd on"
 end
